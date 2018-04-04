63fa28ee3bcd04afee3adfd1d9f78f05904a07d9
moulin <yoshimura-takuya@kayac.com>
2018-04-04 18:19:25 +0900

    renameされたのでfunctions名変更

---
+++ functions/cloudwatch-alarm-to-mackerel/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kayac/cloudwatch-alarm-to-mackerel"
+)
+
+func main() {
+	cwa2mkr.ApexRun()
+}
--- functions/cw-failed-invoke-to-mackerel/main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	cwa2mkr.ApexRun()
-}
